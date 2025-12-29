882560b79d1fa8f7e15595342c4cb4d1582700ae
Tewobsta13 <tewobstal@gmail.com>
2025-12-29 20:45:13 +0000

    adding auth on different routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import userRoutes from  './routes/userRoutes.js'
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
@@ -9,11 +9,11 @@ const PORT = process.env.PORT || 3000;
 
 //json packing middleware
 app.use(express.json());
-
+const userRoutes = require("./routes/userRoute");
+const questionRoutes = require("./routes/questionRoute");
+app.use("/api", authMiddleware, questionRoutes);
 //userRoutes middleware
-app.use('/api',userRoutes)
-
-
+app.use("/api", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: http://localhost:${PORT}`);
