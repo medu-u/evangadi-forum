38506e432a43ad5308af491bbbd30158dca9d0b9
Naol724 <naolgonfa449@gmail.com>
2025-12-29 21:46:27 +0300

    all are solved

--- server/routes/answerRoute.js
+++ server/routes/answerRoute.js
@@ -1,1 +1,10 @@
-c
+import express from "express";
+import { getAnswers } from "../controller/answerController.js";
+
+
+const router = express.Router();
+
+// GET /api/answer/:question_id
+router.get("/answer/:question_id", getAnswers);
+
+export default router;
