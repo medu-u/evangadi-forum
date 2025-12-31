4d69c149c92b6b0a1ef87440ee3e886640fe8d2d
Desalegn Tamirat <desalegntamirateshetu@gmail.com>
2025-12-31 11:20:47 +0300

    implement styling and markdown for messages

--- client/src/Pages/chatBot/chatBot.jsx
+++ client/src/Pages/chatBot/chatBot.jsx
@@ -2,9 +2,10 @@ import { useEffect } from "react";
 import { useChatbot } from "../../hooks/useChatbot";
 import ChatHeader from "../../components/Chatbot/ChatHeader/ChatHeader";
 import ChatInput from "../../components/Chatbot/ChatInput/ChatInput";
+import ChatMessages from "../../components/Chatbot/ChatMessages/ChatMessages";
 
 function ChatBot() {
-  // const { sendMessage, messages } = useChatbot();
+  const { sendMessage, messages } = useChatbot();
   // console.log(messages)
   // useEffect(() => {
   //   sendMessage("what is tailwind").then((res) => console.log(res));
@@ -12,6 +13,7 @@ function ChatBot() {
 
   return <div>
     <ChatHeader isBotTyping={true} />
+    <ChatMessages messages={messages}/>
     <ChatInput />
   </div>;
 }
