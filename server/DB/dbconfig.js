0c74e32dcb922f5b84bb777fe1ba8a1484bbcc5d
Moges <mogesse741@gmial.com>
2026-01-01 09:09:28 +0300

    created file

--- server/DB/dbconfig.js
+++ server/DB/dbconfig.js
@@ -4,7 +4,7 @@ const app = express();
 const dbconnection = mysql.createPool({
   host: "localhost",
   user: "evangadi-admin",
-  password: "evangadi@123admin",
+  password: ".lK*iGiDC92S@QxN",
   database: "evangadi_forum",
   connectionLimit: 11,
 });
