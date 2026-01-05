0825dc08ae55d574b67904199302e0202e5500c8
alemutsion182-cloud <alemutsion182@gmail.com>
2026-01-05 21:33:27 +0300

    style about page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Layout from './components/Layout/Layout'
 
+
 function App() {
 
   return (
--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
 
+import About from "../../Pages/About/About";
+
 function Layout() {
   return (
     <Routes>
-
-
-
+      
+       
+        <Route path="/about" element={<About />} />
+    
     </Routes>
-  )
+  );
 }
 
 export default Layout
