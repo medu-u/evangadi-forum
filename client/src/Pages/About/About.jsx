04c4de1d154bb32df6ab34729c41c0a615a7107d
alemutsion182-cloud <alemutsion182@gmail.com>
2026-01-05 20:34:24 +0300

    add about page content

--- client/src/Pages/About/About.jsx
+++ client/src/Pages/About/About.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import classes from "./about.module.css";
+
 
 function About() {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className={classes.about}>
+      <div className={classes.container}>
+        <p className={classes.label}>About</p>
 
+        <h1 className={classes.title}>Evangadi Networks</h1>
+
+        <p className={classes.text}>
+          No matter what stage of life you are in, whether you’re just starting
+          elementary school or being promoted to CEO of a Fortune 500 company,
+          you have much to offer to those who are trying to follow in your
+          footsteps.
+        </p>
+
+        <p className={classes.text}>
+          Whether you are willing to share your knowledge or you are just
+          looking to meet mentors of your own, please start by joining the
+          network here.
+        </p>
+
+        <button className={classes.button}>HOW IT WORKS</button>
+      </div>
+    </section>
+  );
+};
 export default About
