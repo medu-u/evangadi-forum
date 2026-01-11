3c6737dc2bfa2d6876213dd19084175fd4d2131d
nejat <nej9633@gmail.com>
2026-01-11 18:20:21 +0300

    db

--- server/DB/dbconfig.js
+++ server/DB/dbconfig.js
@@ -3,14 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const dbconnection = mysql.createPool({
-  // host: process.env.HOST,
-  // user: process.env.USER,
-  // password: process.env.PASSWORD,
-  // database: process.env.DATABASE,
-  user: "evangadi-admin",
-  database: "evangadi_forum",
-  host: "localhost",
-  password: "12345678",
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   connectionLimit: 11,
 });
 
