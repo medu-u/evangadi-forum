43a791ca1a7728dc09c57cf72c9fcf5832131630
Berhan-meng <berhanumengesha3@gmail..com>
2026-01-11 11:18:29 +0300

    BaseURL Updated

--- client/src/Api/axiosConfig.js
+++ client/src/Api/axiosConfig.js
@@ -1,11 +1,9 @@
-//
 
 import axios from "axios";
 
 const axiosBase = axios.create({
-  // Replace with your actual Render or local backend URL
   // baseURL: "http://localhost:5500/api",
-  baseURL: "https://evangadiforum.be.birhann.com/`api",
+  baseURL: "https://evangadiforum.be.birhann.com/api",
 });
 
 export default axiosBase;
